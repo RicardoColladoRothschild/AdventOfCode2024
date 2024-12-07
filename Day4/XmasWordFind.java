b062ccb1251a4fa0dae0f9d3dd29388cf400f21c
Ricardo Collado Rothschild <ricardocolladorothschild@gmail.com>
2024-12-06 23:47:19 -0500

    WIP: WORKING ON 4TH DAY

--- Day4/XmasWordFind.java
+++ Day4/XmasWordFind.java
@@ -19,17 +19,34 @@ public class XmasWordFind{
     return totalInRowCounts;
 }
 
+	static int countRowReverse(String oneLine){
+		String regex = "(?=samx)";
+
+			Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+			Matcher matcher = pattern.matcher(oneLine);
+
+			int totalReverseRowCount = 0;
+
+				while(matcher.find()){
+					totalReverseRowCount++;
+				}
+
+				return totalReverseRowCount;
+	}
+
 
 	public static void main(String[] args){
 		List<List<String>> dataInput = new ArrayList<>();
 		String wholeLine = "";
 		int rowCounter = 0;
-		try(BufferedReader br = new BufferedReader(new FileReader("puzzleInput.txt"))){
+		int countRowReverse = 0;
+		try(BufferedReader br = new BufferedReader(new FileReader("inputTest.txt"))){
 			String line = "";
 			while((line = br.readLine()) != null){
 				String[] dataLineArray = line.trim().split("");
-					rowCounter = countRow(line.trim());
-					List<String> subArrayTemp = new ArrayList<>();
+					rowCounter += countRow(line.trim());
+					countRowReverse += countRowReverse(line.trim());
+ 					List<String> subArrayTemp = new ArrayList<>();
 
 					if(dataLineArray.length > 0){
 						for(int i = 0; i < dataLineArray.length; i++){
@@ -44,7 +61,20 @@ public class XmasWordFind{
 		 System.out.println("Ha ocurrido algun error durante la lectura del file"); 
 		} 
 
+		String[][] dataMatriz = new String[dataInput.size()][];
+		for(int i = 0; i < dataInput.size(); i++){
+			dataMatriz[i] = dataInput.get(i).split("");
+
+		}
+
+			int diagonalCount = 0;
+
+				for(int i = 0; i < dataMatriz.length; i++){
+
+				}
+
 		System.out.println("Row counter: " + rowCounter);
+		System.out.println("Row countRowReverse: " + countRowReverse);
 	}
 
  }
