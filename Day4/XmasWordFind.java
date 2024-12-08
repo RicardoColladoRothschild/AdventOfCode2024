59477a79c75f0b949ba70ed36a94703a7314a0b6
Ricardo Collado Rothschild <ricardocolladorothschild@gmail.com>
2024-12-07 19:18:10 -0500

    DAY 4TH FIRST PART done

--- Day4/XmasWordFind.java
+++ Day4/XmasWordFind.java
@@ -34,13 +34,107 @@ public class XmasWordFind{
 				return totalReverseRowCount;
 	}
 
+	static int countDiagonalRight(String[][] dataMatriz){
+
+		int diagonalCount = 0;
+		for (int i = 0; i < dataMatriz.length - 3; i++) {
+		            for (int j = 0; j < dataMatriz[i].length - 3; j++) {
+		                String diagonalWord = dataMatriz[i][j] + dataMatriz[i + 1][j + 1] +
+		                                      dataMatriz[i + 2][j + 2] + dataMatriz[i + 3][j + 3];
+		                if (diagonalWord.equalsIgnoreCase("XMAS")) {
+		                    diagonalCount++;
+		                }
+		            }
+		        }
+
+		        return diagonalCount;
+	}
+
+	static int countDiagonalLeft(String[][] dataMatriz) {
+    int diagonalCount = 0;
+    for (int i = 0; i < dataMatriz.length - 3; i++) { 
+        for (int j = 3; j < dataMatriz[i].length; j++) { 
+            String diagonalWord = dataMatriz[i][j] + dataMatriz[i + 1][j - 1] +
+                                  dataMatriz[i + 2][j - 2] + dataMatriz[i + 3][j - 3];
+            if (diagonalWord.equalsIgnoreCase("XMAS")) {
+                diagonalCount++;
+            }
+        }
+    }
+    return diagonalCount;
+}
+
+  
+    static int countDiagonalReverseUpRight(String[][] dataMatriz) {
+        int count = 0;
+        int rows = dataMatriz.length;
+        int cols = dataMatriz[0].length;
+        for (int i = 3; i < rows; i++) {
+            for (int j = 0; j < cols - 3; j++) {
+                String word = dataMatriz[i][j] + dataMatriz[i - 1][j + 1] + dataMatriz[i - 2][j + 2] + dataMatriz[i - 3][j + 3];
+                if (word.equalsIgnoreCase("XMAS")) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+   
+    static int countDiagonalReverseUpLeft(String[][] dataMatriz) {
+        int count = 0;
+        int rows = dataMatriz.length;
+        int cols = dataMatriz[0].length;
+        for (int i = 3; i < rows; i++) {
+            for (int j = 3; j < cols; j++) {
+                String word = dataMatriz[i][j] + dataMatriz[i - 1][j - 1] + dataMatriz[i - 2][j - 2] + dataMatriz[i - 3][j - 3];
+                if (word.equalsIgnoreCase("XMAS")) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+	static int countVertical(String[][] dataMatriz){
+		int verticalCounter = 0;
+
+
+			for(int i = 0; i < dataMatriz.length - 3; i++){
+				for(int j = 0; j < dataMatriz[i].length; j ++){
+					String xmasWord = dataMatriz[i][j] + dataMatriz[i+1][j] + dataMatriz[i+2][j] + dataMatriz[i+3][j]; 
+					if(xmasWord.equalsIgnoreCase("XMAS")){
+						verticalCounter++;
+					}
+				}
+			}
+			return verticalCounter;
+	}
+
+	static int countVerticalReverse(String[][] dataMatriz) {
+    int verticalCounter = 0;
+
+    for (int j = 0; j < dataMatriz[0].length; j++) { 
+        for (int i = dataMatriz.length - 1; i >= 3; i--) { 
+            String xmasWord = dataMatriz[i][j] + 
+                              dataMatriz[i - 1][j] + 
+                              dataMatriz[i - 2][j] + 
+                              dataMatriz[i - 3][j];
+            if (xmasWord.equalsIgnoreCase("XMAS")) {
+                verticalCounter++;
+            }
+        }
+    }
+    return verticalCounter;
+}
+
 
 	public static void main(String[] args){
 		List<List<String>> dataInput = new ArrayList<>();
 		String wholeLine = "";
 		int rowCounter = 0;
 		int countRowReverse = 0;
-		try(BufferedReader br = new BufferedReader(new FileReader("inputTest.txt"))){
+		try(BufferedReader br = new BufferedReader(new FileReader("puzzleInput.txt"))){
 			String line = "";
 			while((line = br.readLine()) != null){
 				String[] dataLineArray = line.trim().split("");
@@ -63,18 +157,28 @@ public class XmasWordFind{
 
 		String[][] dataMatriz = new String[dataInput.size()][];
 		for(int i = 0; i < dataInput.size(); i++){
-			dataMatriz[i] = dataInput.get(i).split("");
+			dataMatriz[i] = dataInput.get(i).toArray(new String[0]);
 
 		}
 
-			int diagonalCount = 0;
-
-				for(int i = 0; i < dataMatriz.length; i++){
-
-				}
+			int diagonalCount = countDiagonalRight(dataMatriz);
+			int diagonalCountLeft = countDiagonalLeft(dataMatriz);
+			int verticalCounter = countVertical(dataMatriz);
+			int verticalReverse = countVerticalReverse(dataMatriz);
+			int countDiagonalReverse = countDiagonalReverseUpRight(dataMatriz);
+			int countDiagonalReverseU_left = countDiagonalReverseUpLeft(dataMatriz);
 
 		System.out.println("Row counter: " + rowCounter);
 		System.out.println("Row countRowReverse: " + countRowReverse);
+		System.out.println("diagonalCount: " + diagonalCount);
+		System.out.println("diagonalCountLeft: " + diagonalCountLeft);
+		System.out.println("verticalCounter: " + verticalCounter);
+		System.out.println("verticalReverse: " + verticalReverse);
+		System.out.println("countDiagonalReverseUpRight: " + countDiagonalReverse);
+		System.out.println("countDiagonalReverseU_left: " + countDiagonalReverseU_left);
+		
+		int totalCount = rowCounter + countRowReverse + diagonalCount + diagonalCountLeft + verticalCounter + verticalReverse + countDiagonalReverse + countDiagonalReverseU_left;
+		System.out.println("Total: " + totalCount);
 	}
 
  }
